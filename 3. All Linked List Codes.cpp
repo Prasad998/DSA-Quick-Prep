a5ae30fc5dc823a7ae4cc3c296aaba2e222ad3a6
Prasad998 <Prasad24598@gmail.com>
2024-04-29 20:11:59 +0530

    Update and rename 3. All Linked List Codes to 3. All Linked List Codes.cpp

---
+++ 3. All Linked List Codes.cpp
@@ -0,0 +1,15 @@
+
+// Singly linked list node
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+/* Node of a doubly linked list */
+class Node {
+public:
+    int data;
+    Node* next; // Pointer to next node in DLL
+    Node* prev; // Pointer to previous node in DLL
+};
