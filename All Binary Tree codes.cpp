ec104d4cf831391f00987d37a40041b346f20a2d
Prasad998 <Prasad24598@gmail.com>
2024-04-29 17:52:16 +0530

    Update

---
+++ All Binary Tree codes.cpp
@@ -0,0 +1,87 @@
+## Class Representation ##
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+	Node(int val)
+	{
+		data = val ;
+		left = right = NULL ; 
+	}
+
+};
+
+======================================================================
+Pre Order Traversal 
+
+void PreOrder(Node* node)
+{
+	if(node == NULL) return;  
+	
+	cout<< node->val << endl ; 
+	PreOrder(node->left) ; 
+	PreOrder(node->right) ; 
+}
+======================================================================
+In Order Traversal 
+
+void InOrder(Node* node)
+{
+	if(node == NULL) return;  
+	
+	InOrder(node->left) ; 
+	cout<< node->val << endl ; 
+	InOrder(node->right) ; 
+}
+
+======================================================================
+Post Order Traversal 
+
+void PostOrder(Node* node)
+{
+	if(node == NULL) return;  
+	
+	PostOrder(node->left) ; 
+	PostOrder(node->right) ; 
+	cout<< node->val << endl ; 
+}
+
+
+======================================================================
+Level Order Traversal 
+
+vector<int> LevelOrder(Node* root)
+{
+	vector<int> ans ; 
+//BASE Condition 
+	
+	if(root == NULL) return ans ; 
+	
+	queue<Node*> q ; 
+	q.push(root) ;
+	while(!q.empty())
+		{
+			Node* node  = q.front() ; 
+			q.pop() ;
+			if(node->left) q.push(node->left) ;
+			if(node->right) q.push(node->right) ;
+			ans.push_back(node->data) ; 
+		}
+	return ans; 
+}
+
+======================================================================
+Maximum Depth in a Binary Tree
+
+int MaxDepth(Node* root)
+{
+	if(root == NULL) return 0  ; 
+
+	int lh =  MaxDepth(root->left) ;
+	int rh =  MaxDepth(root->right) ;
+
+	return 1+ max(lh + rh) ; 
+}
