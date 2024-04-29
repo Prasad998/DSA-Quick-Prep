072670ac61ce49009b6b8b150ca448d4af837044
Prasad998 <Prasad24598@gmail.com>
2024-04-29 20:01:11 +0530

    Update 2. All Binary Tree codes.cpp

--- 2. All Binary Tree codes.cpp
+++ 2. All Binary Tree codes.cpp
@@ -153,4 +153,72 @@ private:
 };
 
 =================================================================================================
-Maximum Path Sum // Function MaxPath returns [max(lh, rh) + node->val] used to calculate int maxpathsum.
+Check two trees are identical
+
+bool isSameTree(Node* p, Node* q)
+{
+	if( p== NULL || q== NULL) return (p==q) ; 
+
+	return (p->val == q->val)                    //Return statement is a paid actor.
+		&& isSameTree(p->left, q->left) 
+		&& isSameTree(p->right, q->right) ; 
+}
+
+=================================================================================================
+Boundary Traversal
+
+class Solution {
+public:
+    bool isLeaf(Node* root)  return !root->left && !root->right;  // Helper function for checking leaf.
+
+    void addLeftBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->left;
+        while (curr) {
+            if (isLeaf(curr)== false)  res.push_back(curr->data);
+            if (curr->left)  curr = curr->left;
+	    			else curr = curr->right;   
+        }
+    }
+	
+   void addRightBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->right;
+        stack<int> st; 								// Stack because we want rightBoundary elements in reverse order.
+        while (curr) {
+            if (!isLeaf(curr))     stack.push(curr->data);
+            if (curr->right)  curr = curr->right;
+	    			else curr = curr->left;   
+        }
+	   		while(!st.empty())
+		   	{
+			  	 res.push_back(st.top());
+				   st.pop() ; 
+				}
+    }
+    void addLeaves(Node* root, vector<int>& res) {
+        // If the current node is a leaf, add its value to the result
+        if (isLeaf(root)) {
+            res.push_back(root->data);
+            return;
+        }
+        // Recursively add leaves of the left and right subtrees
+        if (root->left)   addLeaves(root->left, res);
+
+        if (root->right)  addLeaves(root->right, res);
+    }
+    // Main function to perform the boundary traversal of the binary tree
+    vector<int> printBoundary(Node* root) {
+        vector<int> res;   
+        if (root == NULL) return res ; 
+        if (!isLeaf(root))  res.push_back(root->data);
+
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+
+        return res;
+    }
+};
+
+
+
+                            
