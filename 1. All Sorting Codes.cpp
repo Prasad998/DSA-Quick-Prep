632ce0c60272e9f75c4628ded408124408488d3b
Prasad998 <Prasad24598@gmail.com>
2024-04-30 06:47:38 +0530

    Update 1. All Sorting Codes.cpp

--- 1. All Sorting Codes.cpp
+++ 1. All Sorting Codes.cpp
@@ -200,3 +200,27 @@ public:
         return -1 ; 
     }
 };
+
+===========================================================================
+
+KADANEs ALGORITHM
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+        
+        if (sum < 0) {   // If sum < 0: discard the sum calculated
+            sum = 0;
+        }
+    }
+    // To consider the sum of the empty subarray, uncomment the following check:
+    //if (maxi < 0) maxi = 0;
+    return maxi;
+}
