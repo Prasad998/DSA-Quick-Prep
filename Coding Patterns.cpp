ac44a86a5bd9b6b61c0c935c8237eeb62edae024
Prasad998 <Prasad24598@gmail.com>
2025-03-19 12:14:46 +0530

    Update Coding Patterns.cpp

--- Coding Patterns.cpp
+++ Coding Patterns.cpp
@@ -1,1 +1,25 @@
-// This file contains the snippets I can use for the corresponding coding patterns.
+This file contains the snippets I can use for the corresponding coding patterns.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+Prefix SUM
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        vec = nums ;
+        for(int i =1 ;i< nums.size();i++)
+        {
+            vec[i]  = vec[i-1]+ nums[i] ; 
+        }
+    }
+    int sumRange(int left, int right) {
+        if(left == 0) return vec[right] ;
+        return vec[right] -  vec[left-1] ; 
+    }
+private: 
+    vector<int> vec ; 
+};
