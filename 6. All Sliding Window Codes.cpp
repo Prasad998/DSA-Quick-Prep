400018beaaa679b31decc74e15904720cab2bac2
Prasad998 <Prasad24598@gmail.com>
2025-04-30 12:34:24 +0530

    Update 6. All Sliding Window Codes.cpp

--- 6. All Sliding Window Codes.cpp
+++ 6. All Sliding Window Codes.cpp
@@ -1,1 +1,60 @@
 
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        // code here 
+        long int sum=0;
+        int i=0;
+        int j=0;
+        long int maxi=INT_MIN;
+        while(j<N)
+        {
+            sum=sum+Arr[j];
+            // Rightmost element me add karo
+            
+            
+            if(j-i+1<K)
+            {
+                j++;
+            }
+            
+            else if(j-i+1==K)
+            {
+              if(sum>maxi)
+              maxi=sum;
+            
+            
+            // leftmost element udao. 
+            sum=sum-Arr[i];
+            
+            // Window slide karo 
+              i++;
+              j++;
+            }
+        
+        }
+        return maxi;
+    }
+};
+// CAN BE BETTER CODED AS ----------------------
+
+class Solution{   
+public:
+   long maximumSumSubarray(vector<int> &Arr,int K){
+        int N = Arr.size() ; 
+        long sum = 0, maxi = INT_MIN;
+        int i = 0, j = 0;
+        while(j < N){
+            sum += Arr[j];
+            if(j - i + 1 < K) j++ ; 
+            
+            else if(j - i + 1 == K){
+                if(sum > maxi) maxi = sum;
+                sum -= Arr[i];
+                i++; 
+                j++;
+            }
+        }
+        return maxi;
+    }
+};
