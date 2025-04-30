e785f8b7b414cde6323dc18fec7a4cf50dc885f6
Prasad998 <Prasad24598@gmail.com>
2025-04-30 12:39:26 +0530

    Update 6. All Sliding Window Codes.cpp

--- 6. All Sliding Window Codes.cpp
+++ 6. All Sliding Window Codes.cpp
@@ -1,34 +1,4 @@
 
-class Solution{   
-public:
-    long maximumSumSubarray(int K, vector<int> &Arr , int N){
-        // code here 
-        long int sum=0;
-        int i=0;
-        int j=0;
-        long int maxi=INT_MIN;
-        
-        while(j<N)
-        {
-            // Sum karna hai apneko
-            sum=sum+Arr[j];
-            // K size tak window banne do...
-            if(j-i+1<K)
-            {
-                j++;
-            }
-            else if(j-i+1==K)            // K size ka window banne ke baad slide marte raho
-            {
-                maxi = max(maxi, sum);
-                sum=sum-Arr[i];  // pehle ka udaao
-                 
-                i++; j++; // Window slide karo 
-            }
-        }
-        return maxi;
-    }
-};
-// CAN BE BETTER CODED AS ----------------------
 
 class Solution{   
 public:
